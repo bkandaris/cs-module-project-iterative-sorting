6f81a4e6fd35aced2e8f7689ab9e2cc4121d0fbe
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2020-09-02 21:22:53 -0400

    mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,31 @@
 def linear_search(arr, target):
     # Your code here
-
-
-    return -1   # not found
+    if target in arr:
+        return arr.index(target) 
+    else:
+        return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
     # Your code here
+    # create values to use
+    start = 0
+    middle = 0
+    total = len(arr) - 1
+    
+    while start <= total:
+        # find our middle index and use it as a starting point for the search (< or >)
+        mid = (total + start) // 2
+        # if the target > mid
+        if arr[mid] < target:
+            start = mid + 1
 
-
+        # if the target < mid
+        elif arr[mid] > target:
+            total = mid - 1
+        # if we got lucky and target is the mid
+        else:
+            return mid
     return -1  # not found
